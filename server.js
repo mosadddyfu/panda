d24b11914eac80bd7deb9cd4f3147a3c2b8c9f6e
Omar-mShehata <omarshehata30.1.2005@gmail.com>
2025-08-28 17:32:12 +0300

    تعديل الازرار

--- server.js
+++ server.js
@@ -801,9 +801,12 @@ app.post('/telegramWebhook', async (req, res) => {
           text: "✅ الموقع يعمل الآن! يمكنك البدء في شراء النجوم من خلال الرابط أدناه:",
           reply_markup: {
             inline_keyboard: [
-              [{ text: "🚀 افتح Panda Store ", url: "t.me/PandaStores_bot/stars" }]
-            ]
-          }
+              [{ text: "🚀 اشترى نجوم من هنا ⭐️", url: "t.me/PandaStores_bot/stars" }]
+            ],
+            inline_keyboard: [
+            [{text: "🚀 اشترى بريميوم من هنا 🫆" , url: "t.me/PandaStores_bot/premium"}  
+          ] 
+              }
         });
       }
     }
